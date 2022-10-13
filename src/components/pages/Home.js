ac1a3dc85ed130e186d71718bfd803aa06fd98b8
Cristiano <crisdb2008@gmail.com>
2022-10-13 20:35:16 -0300

    cart

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,12 +1,12 @@
 import React from "react"
-import user from "../../blue/user.png"
+import cart from "../../blue/cart.png"
 
 function Home(){
     return( 
     <div>
     <h1> Home </h1>
     <h1> Bem vindo ao Supermecado Online</h1>
-    <img src={user} alt="imagem"/>
+    <img src={cart} alt="imagem"/>
     </div>
     )
 }
