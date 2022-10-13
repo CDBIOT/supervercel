6197c402fddc7a3837b165b28bc3c7b03a90e8e6
Cristiano <crisdb2008@gmail.com>
2022-10-13 20:45:28 -0300

    home

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,11 +1,12 @@
 import React from "react"
 import cart from "../../blue/cart.png"
+import styles from './Home.module.css'
 
 function Home(){
     return( 
     <div>
     <h1> Home </h1>
-    <h1> Bem vindo ao Supermecado Online</h1>
+    <h1> Bem vindo ao Supermercado Online</h1>
     <img src={cart} alt="cart"/>
     </div>
     )
