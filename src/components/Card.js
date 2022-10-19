f4b71ea6dfeb5f6f6e5f5c3dc950c45425e796dd
Cristiano <crisdb2008@gmail.com>
2022-10-18 23:56:16 -0300

    changes

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,7 +2,7 @@ import React from "react";
 import Axios from "axios";
 import {useEffect, useState} from 'react';
 
-function Card(){
+function Card(props){
 
     const [products, setProducts] = useState()
 
