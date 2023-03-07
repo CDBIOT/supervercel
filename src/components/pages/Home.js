606e15d2f8ea51dc4b36e1c5819040f971e48d93
Cristiano Bento <crisdb2008@gmail.com>
2023-03-07 20:33:42 -0300

    AWS

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -6,7 +6,7 @@ function Home(){
     return( 
     <div>
     <h1> Home </h1>
-    <h1> Bem vindo ao Supermercado Online</h1>
+    <h1> Bem vindo ao Supermercado AWS</h1>
     <img src={cart} alt="cart"/>
     </div>
     )
