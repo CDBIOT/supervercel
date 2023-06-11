583c08e56794ef4fc4751e394564b6df5af17d99
Cristiano Bento <crisdb2008@gmail.com>
2023-06-11 13:58:29 -0300

    bd_status

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -5,11 +5,34 @@ import styles from './Home.module.css'
 function Home(){
 
     
+const [bd_Status, setBdStatus] = useState();
+const [status, setStatus] = useState(false);
+
+function getStatus(e){
+
+   Axios.get("https://super-server-eta.vercel.app/")
+   .then((response) =>{
+   setStatus(response.data);
+   const data = response.data
+   });
+   {
+   console.log(status)
+   console.log(bd_Status)  
+   }
+}
+
+useEffect(() => {
+  getProducts()
+   
+}, [])
     return( 
     <div>
     <h1> Home </h1>
     <h1> Bem vindo ao Supermercado AWS</h1>
     <img src={cart} alt="cart"/>
+
+    <h1>Status{status}</h1>
+    <h1>BD_Status{bd_Status}</h1>
     </div>
     )
 }
