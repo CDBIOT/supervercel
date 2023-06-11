318a8761ad301cd570155af19a3fa22686434134
Cristiano Bento <crisdb2008@gmail.com>
2023-06-11 17:53:47 -0300

    n

--- src/components/pages/ShowProducts.js
+++ src/components/pages/ShowProducts.js
@@ -21,7 +21,13 @@ const options = {
 function getProducts(e){
 
   //API.get("superExpress", "/products/")
-   Axios.get("https://super-server-eta.vercel.app/products",options)
+   Axios.get("https://super-server-eta.vercel.app/products"),{
+    method: 'GET',
+    cache: 'default',
+    header: { 'Access-Control-Allow-Origin':'*',mode: 'cors',
+    'Content-Type': 'application/json' },
+    redirect: 'follow'
+    };
    .then((response) =>{
    setProducts(response.data);
    const data = response.data
