7487aafd1945ce57bae9344b2844206332ef4365
CDBIOT <crisdb2008@gmail.com>
2023-10-07 15:10:35 -0300

    bod

--- src/components/pages/Cad_Prods.js
+++ src/components/pages/Cad_Prods.js
@@ -40,9 +40,9 @@ const options = {
 await Axios.post("https://super-server-nu.vercel.app/products",options  
    
         ).then((response)=>{
-        response.json(response.data)
-       // console.log(response.data)
-       // console.log(response.body)
+        response.json(response.data.body)
+        console.log(dados)
+       console.log(response)
         })
         .catch (error=> {
             console.log(error.response)
