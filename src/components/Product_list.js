493236112d7050b6acb3095c0eff8368a1ae75c8
Cristiano Bento <crisdb2008@gmail.com>
2024-02-14 18:53:42 -0300

    id

--- src/components/Product_list.js
+++ src/components/Product_list.js
@@ -44,6 +44,8 @@ return (
     <table className={styles.products_table}>
     <tr><tr><th>PostGre</th></tr>
     <th className={styles.th} colSpan={4}>
+        
+    <td width="25%"className={styles.th}>Id</td>
     <td width="25%"className={styles.th}>Produto</td>
     <td width="25%" className={styles.th}>Marca</td>
     <td width="25%" className={styles.th}>Qtd</td>
@@ -56,6 +58,7 @@ return (
          
         products.map((products, index) => (
         <tr key = {index}>
+        <td width="25%"className={styles.td}>{products.id}</td>
         <td width="25%"className={styles.td}>{products.product}</td>
         <td width="25%"className={styles.td}>{products.marca}</td>
         <td width="25%"className={styles.td}>{products.qtd}</td>
